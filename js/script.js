3fa916200e98d67b633fb69232d985eee19c483c
Nicole Mastrostefano <nicole_mas@hotmail.it>
2020-12-17 16:20:42 +0100

    bonus 2

--- js/script.js
+++ js/script.js
@@ -6,16 +6,23 @@ var app = new Vue (
 
     },
 
-    mounted:function(){
+    // methods: {
+    //   sortedArray(){
+    //   return this.albums.sort((a, b) => a.year - b.year );
+    //   }
+    // },
 
+    mounted:function(){
       axios
         .get("https://flynn.boolean.careers/exercises/api/array/music")
-        .then((result)=>this.albums = result.data.response);
-    }
+        .then((result)=>{
+          this.albums = result.data.response;
+          this.albums.sort((a, b) =>
+          a.year - b.year )
+        })
+    },
 
-  }
-)
-
+})
     // "poster": "https://",
     // "title": "Steve Gadd Band",
     // "author": "Steve Gadd Band",
